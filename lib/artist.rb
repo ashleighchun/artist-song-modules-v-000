07377b876533bbf41111b041a55744117661e2f0
Ashleigh Chun <ashleigh.chun@wework.com>
2018-12-15 18:40:23 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,15 +14,15 @@ class Artist
     @songs = []
   end
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+  
 
   def self.all
     @@artists
   end
 
-  
+  def self.find_by_name(name)
+    @@artists.detect{|a| a.name == name}
+  end
 
   def add_song(song)
     @songs << song
